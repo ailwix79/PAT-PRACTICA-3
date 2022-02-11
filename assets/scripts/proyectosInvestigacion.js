e1877b49fd2f029749b33d76ed5db55f0e7c55c6
ailwix79 <90765814+ailwix79@users.noreply.github.com>
2022-02-11 16:33:39 +0100

    Final Changes

--- assets/scripts/proyectosInvestigacion.js
+++ assets/scripts/proyectosInvestigacion.js
@@ -4,4 +4,3 @@
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
 */
 // This file is intentionally blank
-// Use this file to add JavaScript to your project
